30aa101682f991827915cf8b2bc596540ca34147
glaisan <ellenhanx@gmail.com>
2021-02-19 15:58:14 +0300

    ftps almost works, but cannot move files back from server to mac

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-kubectl delete svc nginx-svc
-kubectl delete deploy nginx-deployment
-kubectl delete svc mysql-svc
-kubectl delete deploy mysql-deployment
-kubectl delete svc phpmyadmin-svc
-kubectl delete deploy phpmyadmin-deployment
-kubectl delete svc wordpress-svc
-kubectl delete deploy wordpress-deployment
-# minikube delete
-minikube start --vm-driver=virtualbox
+# kubectl delete svc nginx-svc
+# kubectl delete deploy nginx-deployment
+# kubectl delete svc mysql-svc
+# kubectl delete deploy mysql-deployment
+# kubectl delete svc phpmyadmin-svc
+# kubectl delete deploy phpmyadmin-deployment
+# kubectl delete svc wordpress-svc
+# kubectl delete deploy wordpress-deployment
+# kubectl delete svc ftps-svc
+# kubectl delete deploy ftps-deployment
+minikube delete
+minikube start --driver=virtualbox --memory='2500' --disk-size 3000MB
 eval $(minikube docker-env)
 minikube addons enable metallb
 minikube addons enable dashboard
@@ -20,6 +22,7 @@ docker build -t "nginx_image" nginx
 docker build -t "mysql_image" mysql
 docker build -t "phpmyadmin_image" phpmyadmin
 docker build -t "wordpress_image" wordpress
+docker build -t "ftps_image" ftps
 
 #kubectl apply
 kubectl apply -f configmap.yaml
@@ -27,5 +30,6 @@ kubectl apply -f nginx.yaml
 kubectl apply -f mysql-deployment.yaml
 kubectl apply -f phpmyadmin.yaml
 kubectl apply -f wordpress.yaml
+kubectl apply -f ftps.yaml
 
 minikube dashboard
