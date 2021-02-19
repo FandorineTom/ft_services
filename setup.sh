15caaf17d77d146ea8c6f3654ffeaf3af553a377
glaisan <ellenhanx@gmail.com>
2021-02-19 17:15:58 +0300

    ftps works, telegraf didnt check yet

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# kubectl delete svc nginx-svc
-# kubectl delete deploy nginx-deployment
-# kubectl delete svc mysql-svc
-# kubectl delete deploy mysql-deployment
-# kubectl delete svc phpmyadmin-svc
-# kubectl delete deploy phpmyadmin-deployment
-# kubectl delete svc wordpress-svc
-# kubectl delete deploy wordpress-deployment
-# kubectl delete svc ftps-svc
-# kubectl delete deploy ftps-deployment
-minikube delete
+kubectl delete svc nginx-svc
+kubectl delete deploy nginx-deployment
+kubectl delete svc mysql-svc
+kubectl delete deploy mysql-deployment
+kubectl delete svc phpmyadmin-svc
+kubectl delete deploy phpmyadmin-deployment
+kubectl delete svc wordpress-svc
+kubectl delete deploy wordpress-deployment
+kubectl delete svc ftps-svc
+kubectl delete deploy ftps-deployment
+# minikube delete
 minikube start --driver=virtualbox --memory='2500' --disk-size 3000MB
 eval $(minikube docker-env)
 minikube addons enable metallb
