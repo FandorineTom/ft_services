d26e773813b23ba73b1ef8f259d619870e8fda17
glaisan <ellenhanx@gmail.com>
2021-02-21 03:48:31 +0300

    all containers are working now, but dont restart if the process killed, need to check the last part of the check-list

--- setup.sh
+++ setup.sh
@@ -10,8 +10,14 @@ kubectl delete svc wordpress-svc
 kubectl delete deploy wordpress-deployment
 kubectl delete svc ftps-svc
 kubectl delete deploy ftps-deployment
+kubectl delete svc telegraf-svc
+kubectl delete deploy telegraf-deployment
+kubectl delete svc influxdb-svc
+kubectl delete deploy influxdb-deployment
+kubectl delete svc grafana-svc
+kubectl delete deploy grafana-deployment
 # minikube delete
-minikube start --driver=virtualbox --memory='2500' --disk-size 3000MB
+minikube start --driver=virtualbox
 eval $(minikube docker-env)
 minikube addons enable metallb
 minikube addons enable dashboard
@@ -23,6 +29,9 @@ docker build -t "mysql_image" mysql
 docker build -t "phpmyadmin_image" phpmyadmin
 docker build -t "wordpress_image" wordpress
 docker build -t "ftps_image" ftps
+docker build -t "telegraf_image" telegraf
+docker build -t "influxdb_image" influxdb
+docker build -t "grafana_image" grafana
 
 #kubectl apply
 kubectl apply -f configmap.yaml
@@ -31,5 +40,8 @@ kubectl apply -f mysql-deployment.yaml
 kubectl apply -f phpmyadmin.yaml
 kubectl apply -f wordpress.yaml
 kubectl apply -f ftps.yaml
+kubectl apply -f telegraf.yaml
+kubectl apply -f influxdb.yaml
+kubectl apply -f grafana.yaml
 
 minikube dashboard
