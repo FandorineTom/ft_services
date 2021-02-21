6ce0dcb67919b348b3bbbfe12b9d6a874dd35046
glaisan <ellenhanx@gmail.com>
2021-02-21 21:57:44 +0300

    everything seems to be working ok according to the check-list, need to double check though

--- setup.sh
+++ setup.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-kubectl delete svc nginx-svc
-kubectl delete deploy nginx-deployment
-kubectl delete svc mysql-svc
-kubectl delete deploy mysql-deployment
-kubectl delete svc phpmyadmin-svc
-kubectl delete deploy phpmyadmin-deployment
-kubectl delete svc wordpress-svc
-kubectl delete deploy wordpress-deployment
-kubectl delete svc ftps-svc
-kubectl delete deploy ftps-deployment
-kubectl delete svc telegraf-svc
-kubectl delete deploy telegraf-deployment
-kubectl delete svc influxdb-svc
-kubectl delete deploy influxdb-deployment
-kubectl delete svc grafana-svc
-kubectl delete deploy grafana-deployment
+# kubectl delete svc nginx-svc
+# kubectl delete deploy nginx-deployment
+# kubectl delete svc mysql-svc
+# kubectl delete deploy mysql-deployment
+# kubectl delete svc phpmyadmin-svc
+# kubectl delete deploy phpmyadmin-deployment
+# kubectl delete svc wordpress-svc
+# kubectl delete deploy wordpress-deployment
+# kubectl delete svc ftps-svc
+# kubectl delete deploy ftps-deployment
+# kubectl delete svc telegraf-svc
+# kubectl delete deploy telegraf-deployment
+# kubectl delete svc influxdb-svc
+# kubectl delete deploy influxdb-deployment
+# kubectl delete svc grafana-svc
+# kubectl delete deploy grafana-deployment
+# kubectl delete persistentvolume/influxdb-pv-volume
+# kubectl delete persistentvolumeclaim/influxdb-pv-claim
 # minikube delete
 minikube start --driver=virtualbox
 eval $(minikube docker-env)
@@ -24,24 +26,24 @@ minikube addons enable dashboard
 # minikube addons enable metrics-server
 
 #docker build
-docker build -t "nginx_image" nginx
-docker build -t "mysql_image" mysql
-docker build -t "phpmyadmin_image" phpmyadmin
-docker build -t "wordpress_image" wordpress
-docker build -t "ftps_image" ftps
-docker build -t "telegraf_image" telegraf
-docker build -t "influxdb_image" influxdb
-docker build -t "grafana_image" grafana
+docker build -t "nginx_image" srcs/nginx
+docker build -t "mysql_image" srcs/mysql
+docker build -t "phpmyadmin_image" srcs/phpmyadmin
+docker build -t "wordpress_image" srcs/wordpress
+docker build -t "ftps_image" srcs/ftps
+docker build -t "telegraf_image" srcs/telegraf
+docker build -t "influxdb_image" srcs/influxdb
+docker build -t "grafana_image" srcs/grafana
 
 #kubectl apply
-kubectl apply -f configmap.yaml
-kubectl apply -f nginx.yaml
-kubectl apply -f mysql-deployment.yaml
-kubectl apply -f phpmyadmin.yaml
-kubectl apply -f wordpress.yaml
-kubectl apply -f ftps.yaml
-kubectl apply -f telegraf.yaml
-kubectl apply -f influxdb.yaml
-kubectl apply -f grafana.yaml
+kubectl apply -f srcs/yaml/configmap.yaml
+kubectl apply -f srcs/yaml/nginx.yaml
+kubectl apply -f srcs/yaml/mysql-deployment.yaml
+kubectl apply -f srcs/yaml/phpmyadmin.yaml
+kubectl apply -f srcs/yaml/wordpress.yaml
+kubectl apply -f srcs/yaml/ftps.yaml
+kubectl apply -f srcs/yaml/telegraf.yaml
+kubectl apply -f srcs/yaml/influxdb.yaml
+kubectl apply -f srcs/yaml/grafana.yaml
 
 minikube dashboard
